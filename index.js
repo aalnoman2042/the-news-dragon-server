af6bdcb275178dc86370df53aa94b350ba7d014d
aalnoman <abdullahalnoman2042@gmail.com>
2023-04-27 16:19:37 +0600

    server added

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+ const port = process.env.PORT || 5000;
+
+
+ const categories = require('./Data/categories.json')
+
+
+ app.use(cors())
+ app.get('/', (req,res)=>{
+   res.send("drago is running")
+ })
+
+ app.get('/categories', (req, res)=>{
+    res.send(categories)
+ })
+
+  app.listen(port, ()=>{
+    console.log(`dragon  api is running on port: ${port} `);
+  })
