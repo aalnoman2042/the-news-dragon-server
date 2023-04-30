276c1713765f5a5cee7f27e73092136420af8825
aalnoman <abdullahalnoman2042@gmail.com>
2023-04-30 23:34:00 +0600

    config

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
  const categories = require('./Data/categories.json')
 
+  const news = require('./Data/News.json')
 
  app.use(cors())
  app.get('/', (req,res)=>{
@@ -15,7 +16,27 @@ const cors = require('cors')
  app.get('/categories', (req, res)=>{
     res.send(categories)
  })
+ app.get("/news",(req, res)=>{
+  res.send(news)
+ })
+ app.get("/news/:id",(req, res)=>{
+ const id = req.params.id;
+ console.log(id);
+ const selectedNews = news.find(n => n._id === id)
+ res.send(selectedNews)
+ })
+ app.get('/categories/:id',(req, res)=>{
+  const id = parseInt(req.params.id);
+ if(id === 0){
+  res.send(news)
+ }
+ else{
+   const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+   res.send(categoryNews)
 
+ }
+
+ })
   app.listen(port, ()=>{
     console.log(`dragon  api is running on port: ${port} `);
   })
